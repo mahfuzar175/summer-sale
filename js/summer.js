83c505fff18e317aeb831ae169542935e2c18a76
mahfuzar175 <mahfuzar175@gmail.com>
2023-08-20 13:30:34 +0600

    buttion disabled

--- js/summer.js
+++ js/summer.js
@@ -1,7 +1,24 @@
+let total = 0;
+
 function hanlhleclick(target){
+    // get product to cart
     const selectedItemContainer = document.getElementById('selected-items');
     const itemName = target.childNodes[3].childNodes[3].innerText;
     const li = document.createElement("li");
     li.innerText = itemName;
     selectedItemContainer.appendChild(li);
+
+    // get price
+    const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    total = parseFloat(total) + parseFloat(price);
+    total = total.toFixed(2);
+    console.log(total);
+    // set the total price
+    const firstTotal = (document.getElementById('total').innerText = total);
+    const btnMakePurchase = document.getElementById('btn-make-purchase');
+    if(total > 0){
+        btnMakePurchase.removeAttribute('disabled');
+    }
+
+
 }
